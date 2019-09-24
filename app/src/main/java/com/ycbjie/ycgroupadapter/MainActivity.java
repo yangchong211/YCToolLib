f807e7f5b03cd240003762ff08a3323c3b0274d6
yangchong <yangchong211@163.com>
2019-09-24 14:14:52 +0800

    自定义图片查看器

--- app/src/main/java/com/ycbjie/ycgroupadapter/MainActivity.java
+++ app/src/main/java/com/ycbjie/ycgroupadapter/MainActivity.java
@@ -1,13 +1,28 @@
 package com.ycbjie.ycgroupadapter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_1:
+                startActivity(new Intent(this,FirstActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
